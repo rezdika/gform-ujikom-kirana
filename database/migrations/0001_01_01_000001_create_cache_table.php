47dc2a2a64d7fca9f9bd3d30f34c29dd49475b13
rezdika <rezdika31@gmail.com>
2026-09-19 22:52:38 +0700

    fix: add hasTable checks to prevent duplicate table errors

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cache', function (Blueprint $table) {
+        if (!Schema::hasTable('cache')) Schema::create('cache', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->mediumText('value');
             $table->bigInteger('expiration')->index();
         });
 
-        Schema::create('cache_locks', function (Blueprint $table) {
+        if (!Schema::hasTable('cache_locks')) Schema::create('cache_locks', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->string('owner');
             $table->bigInteger('expiration')->index();
